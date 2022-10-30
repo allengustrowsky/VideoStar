28d9715b19d8d083680717be659801acb09f7340
Allen Gustrowsky <allen@may21.us>
2022-10-30 11:18:28 -0400

    Added most of main-view presentational layout (no logic yet)

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,35 @@ import VideoPaid from './components/VideoPaid'
 import CartWindow from './components/CardWindow'
 import TheaterWindow from './components/TheaterWindow'
 import Hero from './components/Hero'
+import Recommended from './components/Recommended'
 
 function App() {
 
     return (
         <div className="App">
-            <Hero />
+            <header>
+                <Hero />
+            </header>
             <main>
                 <h1 className="browse">Browse</h1>
-                <VideoFree />
-                <VideoPaid />
-                <CartWindow />
-                <TheaterWindow />
+                <Recommended />
+                <section className="gallery">
+                    <div className="galleryHeadContainer">
+                        <h2 className="galleryHeader">Movies We Think You'll Like</h2>
+                        <select name="filterBtn" id="filterBtn">
+                            <option value=""></option>
+                            <option value=""></option>
+                            <option value=""></option>
+                        </select>
+                    </div>
+                    <div className="galleryMain">
+                        {[...Array(16)].map(_ => <VideoPaid />)}
+                    </div>
+                </section>
             </main>
+            <footer>
+                <p>&copy; 2022 Video Star, Inc. All Rights Reserved</p>
+            </footer>
 
         </div>
     )
@@ -31,3 +47,8 @@ export default App
 // -video-free 
 // -cart window
 // theater-mode window
+
+// responsiveness of main gallery (always have full rows)
+// box-shadowing and similar
+//scroll bar distance from cards recommended
+// rremove empty css rules

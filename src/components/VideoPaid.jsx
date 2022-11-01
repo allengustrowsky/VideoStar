353617e2fc95b6ef39f61a325413b0c697f92e38
Allen Gustrowsky <allen@may21.us>
2022-11-01 08:35:51 -0400

    Hover state layout/styling for paid videos

--- src/components/VideoPaid.jsx
+++ src/components/VideoPaid.jsx
@@ -1,20 +1,25 @@
 import dollar from "../assets/dollar.svg"
+import cart from "../assets/cart.svg"
 
 const VideoPaid = (props) => {
     const { key, name, isPurchased, duration, size, price, url } = props
     
     return (
         <div className="videoPaidContainer" >
-            <video type="video/mp4" crossOrigin="true" style={{width: "355px", height: "200px", filter: "blur(7px)"}}>
+            <video className="video" type="video/mp4" crossOrigin="true">
                 <source src={url}/>
             </video>
-            {/* <img className="paidThumbnail thumbnail" type="video/mp4" src={url} alt="movie thumbnail" width="355px" height="200px" /> */}
             <div className="videoInfo">
                 <h3 className="videoTitlePaid">{name}</h3>
                 <p className="videoPrice">${price}</p>
 
             </div>
             <img className="dollar" src={dollar}/>
+            <div className="hoverElement">
+                <div className="hoverIconContainer">
+                    <img className="cart hoverIcon" src={cart} alt="click to add to cart" />
+                </div>
+            </div>
 
         </div>
     )
